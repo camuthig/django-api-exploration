331323f198a13d07e031475c5da3f1a9e0db69a8
Chris Muthig <camuthig@gmail.com>
2025-09-17 08:21:05 -0600

    Add ruff and format

--- src/cbv_api/apps.py
+++ src/cbv_api/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class CbvApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cbv_api'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "cbv_api"
